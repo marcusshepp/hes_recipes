d8bda812c140eb1f4eed202897e24eab105ca527
sheph2mj <sheph2mj@cmich.edu>
2016-04-29 13:18:43 -0400

    model instance method for total

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,30 +1,32 @@
 class RecipesController < ApplicationController
-  # GET /recipes
-  def index
-    @recipes = Recipe.all
-  end
+    # GET /recipes
+    def index
+        @recipes = Recipe.all
+        @foo = [1 , 2]
+    end
 
-  # GET /recipes/:id
-  def show
-  end
+    # GET /recipes/:id
+    def show
+    end
 
-  # GET /recipes/new
-  def new
-  end
+    # GET /recipes/new
+    def new
+        print "hellowrld"
+    end
 
-  # POST /recipes
-  def create
-  end
+    # POST /recipes
+    def create
+    end
 
-  # GET /recipes/:id/edit
-  def edit
-  end
+    # GET /recipes/:id/edit
+    def edit
+    end
 
-  # PUT /recipes/:id
-  def update
-  end
+    # PUT /recipes/:id
+    def update
+    end
 
-  # DELETE /recipes/:id
-  def destroy
-  end
+    # DELETE /recipes/:id
+    def destroy
+    end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients
-  has_many :steps
+    has_many :ingredients
+    has_many :steps
+    def total
+        return self.cook_time + self.prep_time
+    end
 end
