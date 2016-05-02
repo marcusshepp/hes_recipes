37f5b444dbb59c2b206ee768fab87f390aa31788
sheph2mj <sheph2mj@cmich.edu>
2016-05-02 17:28:53 -0400

    going back to parkers

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,10 @@ class Recipe < ActiveRecord::Base
     has_many :ingredients
     has_many :steps
     def total
-        return self.cook_time + self.prep_time
+        if self.cook_time
+            return self.cook_time + self.prep_time
+        else
+            return self.prep_time
+        end
     end
 end
